eb1d5fc026e19b2d03cd64a8526fe8c90b144b76
dscrane <daegancrane@gmail.com>
2022-03-22 15:38:45 -0400

    changes to Panel content and styling

--- client/src/components/Panel/Panel.jsx
+++ client/src/components/Panel/Panel.jsx
@@ -4,19 +4,27 @@ import { Header } from "../Header";
 import { Landing } from "../Landing";
 import { FormContainer } from "../FormContainer";
 import { startGame } from "../../redux/actions";
+import { formInstructions } from "../../lib/instructions";
 import "./panel.css";
 
 const Panel = ({ currentForm, inProgress, startGame }) => {
   const display = inProgress ? <FormContainer /> : <Landing startGame={startGame} />;
+  const panelText = formInstructions[currentForm];
+  const panelTextFormat = currentForm ?
+    <p className="instructions">
+      {
+        panelText.split('\n').map(line => <p>{line}</p>)
+      }
+    </p> :
+    <p className="landing__text">{panelText}</p>;
+
   return (
     <div className="app-panel" data-testid="component-Panel">
       <div className="panel__row panel__row-header">
         <Header currentForm={currentForm} />
       </div>
       <div className="panel__row panel__row-instructions">
-        <span className="instructions">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </span>
+          { panelTextFormat }
       </div>
       <div className="panel__row panel__row-content">{display}</div>
     </div>
