b6b8bca4efc3fb1064c44ceb8a8bfe9523c14198
dscrane <daegancrane@gmail.com>
2022-03-22 15:43:37 -0400

    minor changes from cloning old repo

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import { log } from "../utils/logs.js";
-const connectionURL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`;
 
+const connectionURL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`;
 export default () => {
   log.yellow("[DB]: connecting to database");
   mongoose.connect(
