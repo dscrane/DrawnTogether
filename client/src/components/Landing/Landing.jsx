7cb57aee0d26cb0caf6125ba13daa97e10856ad7
dscrane <daegancrane@gmail.com>
2022-03-22 15:38:23 -0400

    changes to Landing content and styling

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -5,12 +5,12 @@ import "./landing.css";
 export const Landing = ({ startGame }) => {
   return (
     <div className="landing">
-      <p className="landing__text">
-        Circle Generator is the artistic brainchild of Carrie Crane. Moving through the sections of the game will create
-        and alter a collection of circles based on your groups common interest and a variety of personality factors. By
-        the end you will have unique visual of how the members of your group are connected to the common interest that
-        brought you together.
-      </p>
+      {/*<p className="landing__text">*/}
+      {/*  Circle Generator is the artistic brainchild of Carrie Crane. Moving through the sections of the game will create*/}
+      {/*  and alter a collection of circles based on your groups common interest and a variety of personality factors. By*/}
+      {/*  the end you will have unique visual of how the members of your group are connected to the common interest that*/}
+      {/*  brought you together.*/}
+      {/*</p>*/}
       <StartButton startGame={startGame} />
     </div>
   );
