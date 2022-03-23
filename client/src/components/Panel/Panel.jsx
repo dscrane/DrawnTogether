b79f94f61169787124106e1257448d4505960608
dscrane <daegancrane@gmail.com>
2022-03-23 16:09:04 -0400

    removed excess code after restyling

--- client/src/components/Panel/Panel.jsx
+++ client/src/components/Panel/Panel.jsx
@@ -4,24 +4,15 @@ import { Header } from "../Header";
 import { Landing } from "../Landing";
 import { FormContainer } from "../FormContainer";
 import { startGame } from "../../redux/actions";
-import { formInstructions } from "../../lib/instructions";
 import "./panel.css";
 
 const Panel = ({ currentForm, inProgress, startGame }) => {
   const display = inProgress ? <FormContainer /> : <Landing startGame={startGame} />;
-
-  // const panelTextFormat = currentForm ?
-  //
-  //   <p className="landing__text">{panelText}</p>;
-
   return (
     <div className="app-panel" data-testid="component-Panel">
       <div className="panel__row panel__row-header">
         <Header currentForm={currentForm} />
       </div>
-      {/*<div className="panel__row panel__row-instructions">*/}
-      {/*    { panelTextFormat }*/}
-      {/*</div>*/}
       <div className="panel__row panel__row-content">{display}</div>
     </div>
   );
