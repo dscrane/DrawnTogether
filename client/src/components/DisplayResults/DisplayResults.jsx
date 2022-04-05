1562402338a16e8ba0d943ff79bc458a4c0bf960
dscrane <daegancrane@gmail.com>
2022-04-05 14:26:12 -0400

    added classed to DisplayResults

--- client/src/components/DisplayResults/DisplayResults.jsx
+++ client/src/components/DisplayResults/DisplayResults.jsx
@@ -4,10 +4,8 @@ import { ResetButton } from "../../lib/buttons";
 const DisplayResults = ({ endGame }) => {
   return (
     <div className="landing">
-      <p className="landing__text">
-        <p>The final results of your group are displayed to the right.</p>
-        <p>If you would like to play again please hit the "Reset" button!</p>
-      </p>
+      <p className="landing__text">The final results of your group are displayed to the right.</p>
+      <p className="landing__text">If you would like to play again please hit the "Reset" button!</p>
       <ResetButton endGame={endGame} />
     </div>
   );
