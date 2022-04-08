56262ba3911c2ef29c056ee83d6fcfa04da6f5c4
dscrane <daegancrane@gmail.com>
2022-04-08 11:59:05 -0400

    implemented a qr code to the final display

--- client/src/components/DisplayResults/DisplayResults.jsx
+++ client/src/components/DisplayResults/DisplayResults.jsx
@@ -1,80 +1,72 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import FormData from "form-data";
 import * as htmlToImage from "html-to-image";
+import QRCode from 'qrcode';
 import { ActionButton } from "../ActionButton";
 import { endGameEmitter } from "../../socket.io/emitters";
 import { api, dataToFile } from "../../utils";
+import "./displayResults.css";
 
 const DisplayResults = ({ gameId, socket }) => {
-  const [emailSuccess, setEmailSuccess] = useState(false);
-  const [screenshot, setScreenshot] = useState(null);
-  const [email, setEmail] = useState("");
+  const [screenshotStatus, setScreenshotStatus] = useState(false);
+  const [ qrcode, setQrcode ] = useState(null);
 
-  const handleScreenshot = async () => {
-    const dataUrl = await htmlToImage.toPng(document.getElementById("canvas"));
-    const screenshot = await dataToFile(dataUrl, "screenshot.png");
-    setScreenshot(screenshot);
-  };
-
-  const handleEmailChange = (e) => {
-    e.preventDefault();
-    setEmail(e.target.value);
-  };
+  useEffect(() => {
+    const generateQRCode = async () => {
+      const opts = {
+        errorCorrectionLevel: 'H',
+        type: 'image/png',
+        margin: 3,
+        color: {
+          dark:"#8fdbdb",
+          light:"#2b2b2b"
+        }
+      }
+      const urlData = await QRCode.toDataURL(`${process.env.REACT_APP_SERVER}/games/screenshot/${gameId}`, opts);
+      setQrcode(urlData);
+    }
+    generateQRCode();
+  }, [screenshotStatus])
 
   const handleSubmit = async () => {
-    const fileName = `screenshot_${gameId}.png`;
     const form = new FormData();
-
+    const dataUrl = await htmlToImage.toPng(document.getElementById("canvas"));
+    const screenshot = await dataToFile(dataUrl, `screenshot_${gameId}.png`);
     // add data to form object
-    form.append("screenshot", screenshot, fileName);
-    form.append("email", email);
-    form.append("screenshotName", fileName);
+    form.append("screenshot", screenshot);
 
-    const { data } = await api.post("/games/sendScreenshot", form, {
-      headers: { "Content-Type": "multipart/form-data" },
-    });
-    setEmailSuccess(data.emailStatus);
+    const { data } = await api.post("/games/sendScreenshot", form);
+
+    setScreenshotStatus(data.screenshotStatus);
   };
+  const resultsDisplay = !qrcode ? (
+    <>
+      <p className="landing__text landing__text-smaller">
+        If you would like an emailed version of your display click the button below
+      </p>
+      <ActionButton onClick={handleSubmit} buttonType={"screenshot"} text={"Take Screenshot"} />
+    </>
+  ) : (
+    <>
+      <p className="landing__text landing__text-smaller">
+        Scan the code below to download the final display
+      </p>
+      <img className="results__qrcode" src={`${qrcode}`} alt="screenshot-qrcode" />
+    </>
+  );
 
   return (
     <div className="landing">
       <p className="landing__text">The final results of your group are displayed to the right.</p>
-      <p className="landing__text landing__text-smaller">
-        If you would like an emailed version of your display click the button below
-      </p>
-      {emailSuccess ? (
-        <div>
-          <p>The image has been sent.</p>
-          <p>Your email has not been saved and you will not receive additional emails!</p>
-        </div>
-      ) : (
-        <ResultForm
-          screenshot={screenshot}
-          handleScreenshot={handleScreenshot}
-          handleEmailChange={handleEmailChange}
-          handleSubmit={handleSubmit}
-        />
-      )}
+      {resultsDisplay}
       <p className="landing__text">If you would like to play again please hit the "Reset" button!</p>
       <ActionButton onClick={() => endGameEmitter(socket)} buttonType={"restart"} text={"Restart\nGame"} />
     </div>
   );
 };
 
-const ResultForm = ({ screenshot, handleScreenshot, handleSubmit, handleEmailChange }) => {
-  return !screenshot ? (
-    <ActionButton onClick={handleScreenshot} buttonType={"screenshot"} text={"Take Screenshot"} />
-  ) : (
-    <div className="form form__screenshot">
-      <input
-        className="form__control form__control-input form__control-input_screenshot"
-        placeholder="Email..."
-        type="email"
-        onChange={handleEmailChange}
-      />
-      <ActionButton onClick={handleSubmit} buttonType={"send"} text={"Send"} />
-    </div>
-  );
+const ResultForm = ({ handleSubmit, qrcode }) => {
+return
 };
 
 export default DisplayResults;
