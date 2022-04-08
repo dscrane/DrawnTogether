b3b089ba42bd9d710ed6f73ed5f87bfe45a0c99e
dscrane <daegancrane@gmail.com>
2022-04-08 08:53:26 -0400

    updated panel styles and grid color

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-console.log(process.env.NODE_ENV);
-const url = process.env.NODE_ENV === "production" ? "https://dsc-circles.herokuapp.com" : "http://localhost:5500";
+
+const url = process.env.NODE_ENV === "production" ? "https://dsc-circle-server.herokuapp.com" : "http://localhost:5500";
 export const api = axios.create({
   baseURL: url,
 });
