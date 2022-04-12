6866feb13930ce8a0b1426189a74adb35233ea02
dscrane <daegancrane@gmail.com>
2022-04-12 11:47:15 -0400

    updated values for schema

--- client/src/utils/responseSchema.js
+++ client/src/utils/responseSchema.js
@@ -22,7 +22,8 @@ export const responseSchema = {
 const getValue = (vals) => {
   return vals[Math.floor(Math.random() * vals.length)];
 };
-const names = [    "Gonzales",
+const names = [
+  "Gonzales",
   "Harrell",
   "Parker",
   "Owens",
@@ -51,22 +52,23 @@ const names = [    "Gonzales",
   "Moore",
   "Davenport",
   "Michael",
-  "Mack",];
+  "Mack",
+];
 const heightVals = [16, 32, 48];
 const curiosityVals = [27, 36, 18, 9, 0];
-const hairVals = [ 2, 1, 0 ];
+const hairVals = [2, 1, 0];
 const ageVals = [400, 40, 360, 80, 320, 120, 280, 160, 240, 200];
 const dietVals = ["omnivore", "vegetarian", "pescatarian", "vegan"];
 const leaningVals = [18, 45];
 const personalityVals = [60, 31, 18];
-const productivityVals = [2, -1]
+const productivityVals = [2, -1];
 const moneyVals = [0, 1, 2, 3];
 const foodVals = [45, 135, 45, 270];
 const natureVals = ["hollow", "stroke", "ring", "dot"];
 const mediaVals = ["thinner", "thicker", "thin", "thick"];
 const progressVals = ["complimentary", "triadic", "monochromatic", "analogous"];
 const religionVals = ["solid", "round", "dotted", "uneven", "dashed"];
-const cultureVals = [0, 1, 2, 3];
+const cultureVals = [1, 2, 3, 4];
 const colorVals = ["chartreuse", "vermilion", "cobalt", "teal", "kellyGreen", "aubergine"];
 
 export const createResponseSchema = () =>
